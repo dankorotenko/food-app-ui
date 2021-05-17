840dea1e14f2d0664fcce7e0b6a5edead42cb352
Данил Коротенко <dankorotenko@mbpro.local>
2021-05-17 19:52:21 +0200

    initial

---
+++ assets/colors/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    background: '#f9f9fb',
+    textDark: '#313234',
+    primary: '#f5ca48',
+    secondary: '#f26c68',
+    textLight: '#cdcdcd',
+    price:  '#e4723c',
+}
+
+export default colors;
